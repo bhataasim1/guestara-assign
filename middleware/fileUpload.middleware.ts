d3fece2b318b2705f2b874280cd07d6fea2c115f
bhataasim1 <bhataasim700@gmail.com>
2024-08-15 19:21:04 +0530

    chore: Add file upload middleware for handling file uploads

---
+++ middleware/fileUpload.middleware.ts
@@ -0,0 +1,38 @@
+import multer from "multer";
+import path from "path";
+
+
+/// we can modify this to create the directory if it doesn't exist
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+function checkFileType(file: Express.Multer.File, cb: any) {
+  const filetypes = /jpeg|jpg|png|webp/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
+
+export { upload };
